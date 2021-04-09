26c6d27026d586d37dff1a38606001e50cbd2eed
jack0616 <jack0616@hansung.ac.kr>
2021-04-09 11:58:07 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography, Icon } from 'antd';
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+        <Title level={2} >Hansung BOT APP&nbsp;<Icon type="robot" /></Title>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+       
+         <div style={{
+            height: 700, width: 700,
+            border: '3px solid black', borderRadius: '7px'
+        }}>
+            <div style={{ height: 644, width: '100%', overflow: 'auto' }}>
+
+            </div>
+            <input
+                style={{
+                    margin: 0, width: '100%', height: 50,
+                    borderRadius: '4px', padding: '5px', fontSize: '1rem'
+                }}
+                placeholder="Send a message..."
+                type="text"
+            />
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
